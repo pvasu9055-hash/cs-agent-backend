042164632e221e9cca0c5599dc0d9db9d96b211a
Sri Vasu <pvasu9055@gmail.com>
2026-05-03 03:18:50 +0530

    fix message repository

--- src/main/java/online/vasutech/csagent/repository/MessageRepository.java
+++ src/main/java/online/vasutech/csagent/repository/MessageRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface MessageRepository extends JpaRepository<Message, Long> {
-    List<Message> findBySessionIdOrderByCreatedAtAsc(Long sessionId);
+    List<Message> findByChatSessionIdOrderByCreatedAtAsc(Long sessionId);
 }
